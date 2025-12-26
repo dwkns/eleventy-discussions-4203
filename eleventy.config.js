80f01c810d7218ed8aef6fe7bfb6617bbba34311
dwkns <dwkns@me.com>
2025-12-26 16:27:11 +0000

    Testing passthoughcopy without extension.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,6 @@
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 export default (eleventyConfig) => {
-
+  eleventyConfig.addPassthroughCopy("src/mimetype")
 }
 
 export const config = {
